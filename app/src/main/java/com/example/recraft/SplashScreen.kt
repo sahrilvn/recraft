f6692fd9acc4fb32e259960e15a22a3159cf89b1
Sahril <info.sahrilangga@gmail.com>
2023-01-02 15:13:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recraft/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.recraft
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //menghilangkan ActionBar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen)
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }, 2000L) //= 2 detik
+    }
+}
---
+++ app/src/main/java/com/example/recraft/edukasi.kt
@@ -0,0 +1,11 @@
+package com.example.recraft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class edukasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edukasi)
+    }
+}
